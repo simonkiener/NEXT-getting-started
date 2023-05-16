b4c63b1b2832c2c9a018d14387f906695d3236ab
simonkiener <simonkiener@hotmail.com>
2023-05-16 16:38:34 +0200

    add Picture-Example

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,16 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function FirstPost(){
     return (
         <>
             <h1>First Posts</h1>
+            <Image
+                src="/images/Emoji.PNG" // Route of the image file
+                height={144} // Desired size with correct aspect ratio
+                width={144} // Desired size with correct aspect ratio
+                alt="Emoji"
+                />
             <h2>
                 <Link href='/'>Back to Home</Link>
             </h2>
