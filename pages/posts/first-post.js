2b48fee2cb52b8e7c79ec425cbccd7b85cefe11d
simonkiener <simonkiener@hotmail.com>
2023-05-16 16:42:49 +0200

    add MetaData

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
+import Head from "next/head";
 
 export default function FirstPost(){
     return (
         <>
+            <Head>
+                <title>First Post</title>
+            </Head>
             <h1>First Posts</h1>
             <Image
                 src="/images/Emoji.PNG" // Route of the image file
