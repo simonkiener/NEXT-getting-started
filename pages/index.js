872b8451c30a1c9b04551949d52c8b008b93c16d
SimonKiener <simon.kiener@kilchenmann.ch>
2023-05-16 16:20:03 +0200

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -12,6 +13,8 @@ export default function Home() {
       <main>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <br/>
+          Read <Link href='/posts/first-post'>this page</Link>
         </h1>
 
         <p className={styles.description}>
